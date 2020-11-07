051bd9bfccdcb11c1e6237a70c4607c7f8f94822
Alexander Sedov <ouroborosjormungand@gmail.com>
2020-11-07 19:29:12 +0700

    Moved scripts, added turret cone script

---
+++ Assets/Project/Scripts/Turret/TurretVisionCone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TurretVisionCone : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onPlayerContact = null;
+    [SerializeField] private Sprite BurningFlamesOfFieryDeath = null;
+    private SpriteRenderer currentSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            currentSprite.sprite = BurningFlamesOfFieryDeath;
+            onPlayerContact?.Invoke();
+        }
+    }
+}
