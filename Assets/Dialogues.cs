f934fb8defa7914149885513589c2f84722bf8e6
Alexander Sedov <ouroborosjormungand@gmail.com>
2020-11-08 17:49:16 +0700

    added dialogues for bots

--- Assets/Dialogues.cs
+++ Assets/Dialogues.cs
@@ -6,6 +6,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Dialogues", menuName = "ScriptableObjects/DialoguesSO", order = 1)]
 public class Dialogues : ScriptableObject
 {
+    private static Dialogues instance;
+    public static Dialogues Instance { get { return instance; }}
+
     [SerializeField]
     public List<DialogueModel> dialogueList;
+
+    public Dialogues(){
+        instance = this;
+    }
 }
